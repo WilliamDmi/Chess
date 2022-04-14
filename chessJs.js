efe1472bc19e0effbda1c2bad6f11f8f7d96c062
WilliamDmi <williamdemitrienko31@gmail.com>
2022-04-14 13:26:02 +0300

    JS Board

---
+++ chessJs.js
@@ -0,0 +1,46 @@
+window.addEventListener('load',()=> {
+    const newDiv = document.createElement("div"); 
+    newDiv.className="mainBody";
+    document.body.appendChild(newDiv);
+
+    let tbl = document.createElement("table");
+    let tblBody = document.createElement("tbody");
+
+    for(let i = 0 ; i < 10 ; i++ )
+    {
+        let row = document.createElement("tr");
+        for(let j = 0 ; j < 10 ; j++ )
+        {
+            if(i===0 || i===9)
+            {
+                let header = document.createElement("th"); 
+                header.textContent = "1";
+                row.appendChild(header);
+            }
+            else
+            {
+                if(j===0 || j===9)
+                {
+                    let header = document.createElement("th"); 
+                    header.textContent = "1";
+                    row.appendChild(header);
+                }
+                else
+                {
+                    let cell = document.createElement("td");
+                    if( (j+i)%2 === 1)
+                    {
+                        cell.className="tdWhite";
+                    }
+                    else cell.className="tdBlack";
+                    row.appendChild(cell);
+                }
+            }   
+
+        }
+        tblBody.appendChild(row);
+    }
+    tbl.appendChild(tblBody);
+    newDiv.appendChild(tbl);
+
+});
